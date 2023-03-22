16a3f619f48518ff3c7a103f387328061f48faa8
edsml-hz822 <112737829+edsml-hz822@users.noreply.github.com>
2023-03-22 23:46:36 +0000

    Add files via upload

---
+++ Filter class/filter.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <string>
+#include "Image.h"
+
+// The Filter class provides various image filtering operations.
+class Filter
+{
+public:
+    // Constructor: Initializes a Filter object with the given image, kernel size, sigma, and depth values.
+    Filter(Image& image, int k_size = 0, float sig = 1.0f, int d =1);
+    
+    // Destructor: Frees the memory allocated for the corrected_img buffer.
+    ~Filter();
+
+    // Applies a grayscale filter to the image.
+    void ApplyGrayScale();
+    
+    // Applies an automatic color balance filter to the image.
+    void AutoColourBalance();
+    
+    // Adjusts the brightness of the image by the specified amount.
+    void Brightness(int brightness);
+    
+    // Applies the Sobel edge detection filter to the image.
+    void Sobel();
+    
+    // Applies the Prewitt edge detection filter to the image.
+    void Prewitt();
+    
+    // Applies a median blur filter to the image.
+    void median_blur();
+    
+    // Applies a box blur filter to the image.
+    void box_blur();
+    
+    // Applies a Gaussian blur filter to the image.
+    void gaussian_blur();
+    
+    // Applies a median blur filter to a 3D image.
+    void median_blur_3d();
+    
+    // Applies a Gaussian blur filter to a 3D image.
+    void gaussian_blur_3d();
+    
+    // Saves the filtered image to a file at the specified path.
+    void SaveImg(const std::string& s_path);
+
+    // Returns a pointer to the filtered image data.
+    unsigned char* get_corrected_img() const;
+
+private:
+    Image& image;
+    int kernel_size;
+    float sigma;
+    int width, height, channels;
+    size_t img_size;
+    int depth;
+    unsigned char* corrected_img;
+};
